44588f8e2c4e0857ce9b6eb3a714bd3cbe3e7e66
Bruno Vicente <brunovicente32@gmail.com>
2021-01-21 06:16:38 -0300

    estilos menu

--- src/components/ListCities.js
+++ src/components/ListCities.js
@@ -1,25 +1,60 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getNewCity } from '../actions/actions'
+import styles from 'styled-components'
+
+const Item = styles.li`
+  border: 1px solid transparent;
+  background: none;
+  height: 64px;
+  width: 351px;
+  margin: 0 auto;
+  padding: 22px 12px;
+  color: #E7E7EB;
+  font-family: Raleway;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 19px;
+  letter-spacing: 0em;
+  text-align: left;
+  display: flex;
+  justify-content:space-between;
+  cursor: pointer;
+  span {
+    color: #616475;
+    display: none;
+  }
+  &:hover {
+    border: 1px solid #616475;
+    span{
+      display: block;
+    } 
+  }
+`
 
 export const ListCities = () => {
   const { cities } = useSelector((state) => state)
   const dispatch = useDispatch()
 
   return (
-    <div style={{ backgroundColor: 'blue' }}>
-      <ol>
+    <div>
+      <ul>
         {cities &&
           cities.map((city) => (
-            <li
+            <Item
               onClick={() =>
                 dispatch(getNewCity(city.coord.lat, city.coord.lon))
               }
+
             >
               {`${city.name}, ${city.sys.country}`}
-            </li>
+              <span class="material-icons">
+                navigate_next
+              </span>
+            </Item>
           ))}
-      </ol>
+      </ul>
     </div>
   )
 }
